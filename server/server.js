3a959105441620d1146a71c16ea2dde9beb96295
Alexandra Holden <alexandraholden@Alexandras-MacBook-Air.local>
2022-05-17 16:34:11 -0700

    implemented apollo server with express server middleware

--- server/server.js
+++ server/server.js
@@ -1,11 +1,32 @@
 const express = require('express');
 const path = require('path');
-const db = require('./config/connection');
-const routes = require('./routes');
 
+// import apollo server
+const { ApolloServer } = require('apollo-server-express');
+
+// import auth middleware
+const {authMiddleware} = require('./utils/auth');
+
+// db connection 
+const db = require('./config/connection');
+
+// const routes = require('./routes');
+
+// express server
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+// apollo server
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware
+});
+
+// apply apollo server with express
+server.applyMiddleware({ app });
+
+// middleware parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -14,8 +35,11 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
+// app.use(routes);
 
 db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  app.listen(PORT, () => {
+    console.log(`🌍 Now listening on localhost:${PORT}`);
+    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+  });
 });
